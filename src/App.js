9f113cdd3bc18535680cb5a4e87a3fd45743c9ae
Swizec Teller <swizec@swizec.com>
2016-12-22 09:14:39 -0800

    Loading data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-
+import * as d3 from 'd3';
+import _ from 'lodash';
 
 import Preloader from './components/Preloader';
+import { loadAllData } from './DataHandling';
 
 class App extends Component {
     state = {
-        techSalaries: []
+        techSalaries: [],
+        countyNames: [],
+        medianIncomes: []
+    }
+
+    componentWillMount() {
+        loadAllData(data => this.setState(data));
     }
 
     render() {
@@ -16,8 +24,8 @@ class App extends Component {
         }
 
         return (
-            <div className="App">
-
+            <div className="App container">
+                <h1>Loaded {this.state.techSalaries.length} salaries</h1>
             </div>
         );
     }
